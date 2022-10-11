c1fd16c8746ffa0f17455dc7700547fd086dd41a
Adam Pithenwala <adam@pop-os.localdomain>
2022-10-11 23:23:37 +0530

    Merg sort

---
+++ Sorting/MergeSort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+
+void print_array(int arr[], int n) {
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+}
