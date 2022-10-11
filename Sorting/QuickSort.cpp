e1a7e44072575f77165f86b1bc303a6e5f6a729f
Adam Pithenwala <adam@pop-os.localdomain>
2022-10-11 22:50:13 +0530

    Quick sort added

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 6, 1, 7, 3, 8, 45, 96, 12};
+    print_array(arr, 9);
+    quickSort(arr, 0, 9);
+    print_array(arr, 9);
+}
